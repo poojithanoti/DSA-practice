8809f4c7add0ef3954ec6c5d2d41d968ce10bedc
Noti Poojitha <poojithanoti26@gmail.com>
2026-03-14 22:27:18 +0530

    Added stack implementation in C.
    This program helps you in implementing stack using an array in C.

---
+++ stack_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+void push(int x) {
+    if(top == MAX-1)
+        printf("Stack Overflow\n");
+    else {
+        top++;
+        stack[top] = x;
+    }
+}
+void pop() {
+    if(top == -1)
+        printf("Stack Underflow\n");
+    else {
+        printf("Popped element: %d\n", stack[top]);
+        top--;
+    }
+}
+void display() {
+    int i;
+    if(top == -1)
+        printf("Stack is empty\n");
+    else {
+        for(i = top; i >= 0; i--)
+            printf("%d\n", stack[i]);
+    }
+}
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+    return 0;
+}
